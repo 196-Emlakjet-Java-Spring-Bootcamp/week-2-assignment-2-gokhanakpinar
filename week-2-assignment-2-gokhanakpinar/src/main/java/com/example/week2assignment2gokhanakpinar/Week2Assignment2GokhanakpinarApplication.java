deff18ede89a53523b7c54ba0c8066ed5d2b38dd
Gökhan Akpınar <gkhnkpnr@icloud.com>
2022-06-02 21:34:59 +0300

    First Commit

---
+++ week-2-assignment-2-gokhanakpinar/src/main/java/com/example/week2assignment2gokhanakpinar/Week2Assignment2GokhanakpinarApplication.java
@@ -0,0 +1,13 @@
+package com.example.week2assignment2gokhanakpinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2Assignment2GokhanakpinarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2Assignment2GokhanakpinarApplication.class, args);
+    }
+
+}
